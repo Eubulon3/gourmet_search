612a5222d70a995966a9e28471f977e172394f11
chashu <chashu@chashus-MacBook-Pro.local>
2024-02-14 11:09:55 +0900

    基盤完成

--- gsapp/urls.py
+++ gsapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name = "index"),
     path("search/", views.SearchView.as_view(), name = "search"),
     path("gourmet_list/", views.gourmet_list, name = "gourmet_list"),
+    path("detail/<str:id>/", views.detail_view, name = "detail"),
 ]
--- gsapp/views.py
+++ gsapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from django.views import generic
 import requests
+import os
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
@@ -8,24 +10,25 @@ class IndexView(generic.TemplateView):
 class SearchView(generic.TemplateView):
     template_name = "search.html"
 
-# class GourmetListView(generic.ListView):
-#     template_name = "gourmet_list.html"
 
 def gourmet_list(request):
 
     api_endpoint = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
-    api_key = "1b1c0aede467a3f4"
+    api_key = os.environ.get("API_KEY")
 
-    keyword = request.GET.get("keyword", "")
-    # search2 = request.GET.get("search2", "")
+    keywords = request.GET.get("keywords", "")
+    budget = request.GET.get("budget", "")
+    range = request.GET.get("range", "")
+    lat = ""
+    ing = ""
 
     params = {
         "key": api_key,
-        "keyword": keyword,
+        "keyword": keywords,
+        "count": 50,
         "format":"json",
     }
 
-    print(params)
 
 
     response = requests.get(api_endpoint, params=params)
@@ -34,10 +37,48 @@ def gourmet_list(request):
     if response.status_code == 200:
         data = response.json()
 
+        gourmet_list = data["results"]["shop"]
+
+        per_page = 10
+        page_num = request.GET.get("page", 1)
+        paginator = Paginator(gourmet_list, per_page)
+        page_obj = paginator.get_page(page_num)
+
         context = {
-            "gourmet_list": data["results"]["shop"],
+            "page_obj": page_obj,
         }
+
         return render(request, "gourmet_list.html", context)
+    else:
+        # エラーメッセージを表示
+        pass
+
+
+def detail_view(request, id):
+
+    api_endpoint = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
+    api_key = "1b1c0aede467a3f4"
+
+
+    params = {
+        "key": api_key,
+        "id": id,
+        "format":"json",
+    }
+
+    response = requests.get(api_endpoint, params=params)
+    if response.status_code == 200:
+        data = response.json()
+
+        gourmet_detail = data["results"]["shop"][0]
+
+        print(gourmet_detail)
+
+        context = {
+            "detail": gourmet_detail,
+        }
+
+        return render(request, "detail.html", context)
     else:
         # エラーメッセージを表示
         pass
